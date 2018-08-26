d94049a08943bc820b59b5e1832c8498eee5ddca
Altin Selimi <altinselimi@msn.com>
2018-08-26 19:01:27 +0200

    fix baseurl

--- src/main.js
+++ src/main.js
@@ -5,11 +5,12 @@ import Axios from 'axios';
 import './routes';
 
 Axios.defaults.headers.common.Accept = 'application/json';
-let baseURL = `$https://monimo-api.herokuapp.com/`;
+let baseURL = `https://monimo-api.herokuapp.com/`;
 if(process.env.NODE_ENV !== 'production') {
 	baseURL = 'http://localhost:6069';
 }
 console.log('Env:', process.env.NODE_ENV);
+console.log('BaseUrl:', baseURL);
 Axios.defaults.baseURL = baseURL;
 Vue.$http = Axios;
 
