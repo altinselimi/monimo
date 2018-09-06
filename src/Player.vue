05fda04b4402fcc3023966c9fddcb11d9c4cc82f
Shpetim Selaci <shselaci3@gmail.com>
2018-09-06 14:15:26 +0200

    videoplayer environment based conditions

--- src/Player.vue
+++ src/Player.vue
@@ -1,43 +1,64 @@
 <template>
-	<div class="player">
-		<headerr />
-		<!-- <iframe sandbox="allow-scripts"  :src="iframe_src" v-if="iframe_src" width="100%" height="100%" frameborder="0" scrolling="no" allowfullscreen="allowfullscreen"></iframe> -->
-		<webview  :src="iframe_src" v-if="iframe_src" disablewebsecurity style="width:100%;height:100%"></webview>
-	</div>
+    <div class="player">
+        <headerr/>
+        <webview :src="iframe_src" v-if="isElectron" disablewebsecurity style="width:100%;height:100%"></webview>
+        <iframe sandbox="allow-scripts" :src="iframe_src" v-else width="100%" height="100%" frameborder="0" scrolling="no" allowfullscreen="allowfullscreen">
+        </iframe>
+    </div>
 </template>
 <script>
-export default {
-	beforeMount() {
-		let link = this.$route.params.src;
-		this.iframe_src = atob(this.$route.params.src);
-	},
-	mounted(){
-		setTimeout(()=>{
-			let webview = document.querySelector("webview");
-			webview.sendInputEvent({type:'mouseDown', x:5, y: 230, button:'left', clickCount: 1});
-			webview.sendInputEvent({type:'mouseUp', x:5, y: 230, button:'left', clickCount: 1});
-			console.log("working");
-		},3000)
-	},
-	components: {
-		headerr: () =>
-			import ('./components/header.vue'),
-	},
-	data: () => ({
-		iframe_src: null,
-	}),
-}
+    export default {
+        created() {
+            let link = this.$route.params.src;
+            this.iframe_src = atob(this.$route.params.src);
+            if (!this.iframe_src) this.$router.push('/');
+        },
+        mounted() {
+        	if(!this.isElectron) return;
+            setTimeout(() => {
+                let webview = document.querySelector("webview");
+                webview.sendInputEvent({
+                    type: 'mouseDown',
+                    x: 5,
+                    y: 230,
+                    button: 'left',
+                    clickCount: 1
+                });
+                webview.sendInputEvent({
+                    type: 'mouseUp',
+                    x: 5,
+                    y: 230,
+                    button: 'left',
+                    clickCount: 1
+                });
+                console.log("working");
+            }, 3000)
+        },
+        methods: {
+            isElectron() {
+                return navigator.userAgent.toLowerCase().indexOf('electron/') > -1;
+            }
+        },
+        components: {
+            headerr: () =>
+                import ('./components/header.vue'),
+        },
+        data: () => ({
+            iframe_src: null,
+        }),
+    }
 </script>
 <style lang="scss">
-.player {
-	width: 100%;
-	height: 100%;
-}
+    .player {
+        width: 100%;
+        height: 100%;
+    }
 
-iframe, webview {
-	padding-top: 0px!important;
-	width: 100%;
-	height: 100%;
-	border: none;
-}
+    iframe,
+    webview {
+        padding-top: 0px!important;
+        width: 100%;
+        height: 100%;
+        border: none;
+    }
 </style>
