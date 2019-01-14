e6891492b7e6c8465d90b069d90973687c8d887d
CaedenZ <Fengye.16@ichat.sp.edu.sg>
2019-01-14 16:38:38 +0800

    abc

--- src/api.js
+++ src/api.js
@@ -13,13 +13,21 @@ export default {
 		console.log('params string:', params_string);
 		let anime_url = `https://www.masterani.me/api/anime/filter?${params_string}`;
 		return new Promise((resolve, reject) => {
-			Vue.$http.get(anime_url).then(response => { resolve(response.data) }).catch(err => { reject(err) });
+			Vue.$http.get(anime_url).then(response => {
+				resolve(response.data)
+			}).catch(err => {
+				reject(err)
+			});
 		});
 	},
 	getReleases: (req) => {
 		let url = 'https://www.masterani.me/api/releases';
 		return new Promise((resolve, reject) => {
-			Vue.$http.get(url).then(response => { resolve(response.data) }).catch(err => { reject(err) });
+			Vue.$http.get(url).then(response => {
+				resolve(response.data)
+			}).catch(err => {
+				reject(err)
+			});
 		});
 	},
 	animeDetails: (req) => {
@@ -27,7 +35,11 @@ export default {
 		let anime_url = `https://www.masterani.me/api/anime/${req.anime_id}/detailed`;
 		if (!req.anime_id) throw new Error('missing url');
 		return new Promise((resolve, reject) => {
-			Vue.$http.get(anime_url).then(response => { resolve(response.data) }).catch(err => { reject(err) });
+			Vue.$http.get(anime_url).then(response => {
+				resolve(response.data)
+			}).catch(err => {
+				reject(err)
+			});
 		});
 	},
 	videoLinks: (req) => {
@@ -46,11 +58,16 @@ export default {
 					type: mirror.type === 1 ? 'sub' : 'dub',
 					name: mirror.host.name,
 					link: `${mirror.host.embed_prefix}${mirror.embed_id}${mirror.host.embed_suffix}`,
-				}));//.filter(video => !video.link.includes('openload') && !video.link.includes('streamango'));
+				})); //.filter(video => !video.link.includes('openload') && !video.link.includes('streamango'));
 				//openload and streamango giving troubles rn. They have scripts that are clearing the console logs, which we use to communicate with parent component.
 				let subs = links.filter(link => link.type === 'sub');
 				let dubs = links.filter(link => link.type === 'dub');
-				resolve({ subs, dubs, slug: req.slug, 'episode': req.episode });
+				resolve({
+					subs,
+					dubs,
+					slug: req.slug,
+					'episode': req.episode
+				});
 				//section.parentNode.removeChild(section);
 				section.parentNode.removeChild(section);
 			}).catch(err => reject(err));
